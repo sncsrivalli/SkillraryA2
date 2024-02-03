c30881994303a22eb6e631f23fb9f2ed2c0c8f74
TRACK QJSPIDERS <TRACK QJSPIDERS@DESKTOP-2L70A79>
2024-02-03 13:28:55 +0530

    Added comment to AddCategoryTest.java

--- src/test/java/testScripts/AddCategoryTest.java
+++ src/test/java/testScripts/AddCategoryTest.java
@@ -7,7 +7,7 @@ import org.testng.asserts.SoftAssert;
 
 import genericUtilities.BaseClass;
 import genericUtilities.IConstantPath;
-
+//This test verifies if user is able to create Category
 public class AddCategoryTest extends BaseClass {
 
 	@Test
